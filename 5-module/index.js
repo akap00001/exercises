3102dd65bc880f3f5664f5f3bd0446150b08a756
Akua <akap0001@stud.kea.dk>
2026-02-02 08:50:02 +0100

    Add Guessing Game and Rock, Paper, Scissors implementations with HTML, CSS, and JavaScript

---
+++ 5-module/index.js
@@ -0,0 +1,9 @@
+import { getRandomNumber } from '../utils/utils_lib.js';
+
+function (?) {
+    return document.querySelector(?)
+}
+ 
+console.log("Random number:", getRandomNumber(3));
+
+document.querySelector(".mitPTag").innerHTML = "MODULES";
