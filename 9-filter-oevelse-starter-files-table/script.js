eead5e9bae906bbaea976748178b995a6a38babd
Akua <akap0001@stud.kea.dk>
2026-02-05 17:07:49 +0100

    tilføjer filter-øvelsen

--- 9-filter-oevelse-starter-files-table/script.js
+++ 9-filter-oevelse-starter-files-table/script.js
@@ -14,8 +14,34 @@ const tbodyPointer = document.querySelector("tbody");
 showTheseVehicles(vehicles);
 
 function showTheseVehicles(arr) {
+  tbodyPointer.innerHTML = "";
+
   arr.forEach((each) => {
-    tbodyPointer.innerHTML += `<tr>
+
+  //   let fuel = "";
+  //   let stops = "";
+  //   let ownedBy = "";
+  //   let isElectric = "";
+  //   let isTandem = "";
+
+  //   if (each.fuel) {
+  //     fuel = each.fuel;
+  //   }
+  //   if (each.stops) {
+  //     stops = each.stops;
+  //   }
+  //   if (each.ownedBy) {
+  //     ownedBy = each.ownedBy;
+  //   }
+  //   if (each.isElectric === true) {
+  //     isElectric = "Yes";
+  //   }
+  //   if (each.isTandem === true) {
+  //     isTandem = "Yes";
+  //   }
+    
+    tbodyPointer.innerHTML +=
+`<tr>
   <td>${each.type}</td>
   <td>${each.fuel}</td>
   <td>${each.passengers}</td> 
@@ -25,4 +51,80 @@ function showTheseVehicles(arr) {
   <td>${each.isTandem}</td>
 </tr>`;
   });
+
 }
+
+// FILTRERINGER
+// 1. en der viser alle el drevne fartøjer
+const elFartøjer = vehicles.filter((each) => {
+  return each.isElectric === true;
+});
+// console.log(elFartøjer)
+
+// 2. en der viser alle fartøjer med mere end 2 sæder
+const flereSæder = vehicles.filter((each) => {
+  return each.passengers > 2;
+});
+// console.log(flereSæder)
+
+// 3. alle fartøjer el-drevne fartøjer ejet af Jonas
+const elJonas = vehicles.filter((each) => {
+  return each.ownedBy === "Jonas";
+});
+// console.log(elJonas)
+
+// 4. alle rugbrøds drevne fartøjer med plads til mere end en.
+const rugbrødsFartøj = vehicles.filter((each) => {
+  return each.fuel === "Rugbrød" && each.passengers > 1;
+});
+// console.log(rugbrødsFartøj)
+
+
+// FILTRERINGSKNAPPER
+// Flyt filtreringerne over på 4 knapper der viser de filtreringer der før var hardcodede + 1 knap til at vise alle (ufiltreret)
+const allBtn = document.getElementById("all");
+const elBtn = document.getElementById("el");
+const seatsBtn = document.getElementById("seats");
+const jonasBtn = document.getElementById("jonas");
+const rugBtn = document.getElementById("rug");
+
+function showFiltered() {
+  showTheseVehicles(vehicles);
+}
+
+function showElectric() {
+  const elFartøjer = vehicles.filter((each) => {
+  return each.isElectric === true;
+});
+  showTheseVehicles(elFartøjer)
+}
+
+function showSeats() {
+  const flereSæder = vehicles.filter((each) => {
+  return each.passengers > 2;
+});
+  showTheseVehicles(flereSæder)
+}
+
+function showJonas() {
+  const elJonas = vehicles.filter((each) => {
+  return each.ownedBy === "Jonas";
+});
+  showTheseVehicles(elJonas)
+}
+
+function showRugbrødSeats() {
+  const rugbrødsFartøj = vehicles.filter((each) => {
+  return each.fuel === "Rugbrød" && each.passengers > 1;
+});
+  showTheseVehicles(rugbrødsFartøj)
+}
+
+allBtn.addEventListener("click", showFiltered);
+elBtn.addEventListener("click", showElectric);
+seatsBtn.addEventListener("click", showSeats);
+jonasBtn.addEventListener("click", showJonas);
+rugBtn.addEventListener("click", showRugbrødSeats);
+
+
+
